8449b9ea70f55b02d4f31485ee4835af5252691b
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2025-08-09 01:06:34 -0300

    Login e Registro do Blog

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,88 @@
+﻿using Blog.Models;
+using Blog.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new UserViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(UserViewModel userVM)
+        {
+            if (ModelState.IsValid) return View(userVM);
+
+            var user = await _userManager.FindByNameAsync(userVM.UserName);
+
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, userVM.UserPassword, false, false);
+
+                if (result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(userVM.ReturnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    return Redirect(userVM.ReturnUrl);
+                }
+            }
+            ModelState.AddModelError("", "Falha ao realizar o login!");
+            return View(userVM);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(UserViewModel userVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                User user = new User { UserName = userVM.UserName };
+                var result = await _userManager.CreateAsync(user, userVM.UserPassword);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Falha ao fazer o registro");
+                }
+            }
+            return View(userVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            HttpContext.User = null;
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ ViewModels/UserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required(ErrorMessage = "Informe um nome")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Informe uma senha")]
+        [DataType(DataType.Password)]
+        public string UserPassword { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
